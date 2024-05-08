6c4daf2cdfdc9dc7ddeb4c3554bbfce602061fe7
dafhome <d.fernalvarez@gmail.com>
2024-05-08 18:39:01 +0200

    commit

--- 1.-CRUDAlimentos/src/Metodos.java
+++ 1.-CRUDAlimentos/src/Metodos.java
@@ -19,4 +19,22 @@ public class Metodos {
         }
         return valor;
     }
-    pub
+    public static int validarInt (Scanner scan){
+        boolean validar = false;
+        int valor=0;
+        while (!validar) {
+
+            if (scan.hasNextInt()) {
+                validar = true;
+                valor = scan.nextInt();
+                scan.nextLine();
+            } else {
+                System.out.print("Introduce un valor con formato válido:");
+                scan.next();
+            }
+
+        }
+        return valor;
+    }
+    
+}
